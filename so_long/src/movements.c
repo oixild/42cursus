8d574e22b0b6578bd835087ea7225ab469de6f2a
David Sabater 42 <dsabater@student.42barcelona.com>
2023-11-30 12:33:13 +0100

    updates

--- so_long/src/movements.c
+++ so_long/src/movements.c
@@ -6,14 +6,33 @@
 /*   By: dsabater <dsabater@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 10:53:04 by dsabater          #+#    #+#             */
-/*   Updated: 2023/11/30 10:54:15 by dsabater         ###   ########.fr       */
+/*   Updated: 2023/11/30 12:32:28 by dsabater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
 
-move_up(t_game g)
+move_up(t_game *g)
 {
-
+	// Adaptar al codigo con esta logica
+/* 	int i;
+	i = 0;
+	while (i++ < ft_strlen(g->str_line))
+	{
+		if (g->str_line[i] == 'P')
+		break ;
+	}
+	if (g->str_line[i - g->wid] == 'C')
+		g->col_cnt++;
+	if (g->str_line[i - g->wid] == 'E' && g->all_col == g->col_cnt)
+		endgame(g);
+	else if (g->str_line[i - g->wid] != '1' && g->str_line[i - g->wid] != 'E')
+	{
+		g->str_line[i] = '0';
+		g->str_line[i - g->wid] = 'P';
+		g->walk_cnt++;
+		printf("%d\n", g->walk_cnt);
+		setting_img(g);
+	} */
 }
