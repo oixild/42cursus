9083994a87a89097d401b3912658a8a753001a48
David Sabater 42 <dsabater@student.42barcelona.com>
2023-12-11 13:11:19 +0100

    finish-only-norm-correct

--- so_long/src/flood_fill.c
+++ so_long/src/flood_fill.c
@@ -6,34 +6,36 @@
 /*   By: dsabater <dsabater@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 09:48:54 by dsabater          #+#    #+#             */
-/*   Updated: 2023/12/11 13:00:27 by dsabater         ###   ########.fr       */
+/*   Updated: 2023/12/11 13:10:46 by dsabater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
-void	map_flood_fill(char *mapcpy, int width, int i, int *find, char target)
+void	map_flood_fill(char *mapcpy, int width, int i, int *find)
 {
 	if (mapcpy[i] == '1' || mapcpy[i] == '*' || *find == 0 || mapcpy[i] == 'E')
 	return ;
-	if (mapcpy[i] == target)
+	if (mapcpy[i] == 'E')
 		*find = *find - 1;
 	mapcpy[i] = '*';
 	if (*find > 0)
 	{
-		map_flood_fill(mapcpy, width, i - width, find, target);
-		map_flood_fill(mapcpy, width, i + 1, find, target);
-		map_flood_fill(mapcpy, width, i + width, find, target);
-		map_flood_fill(mapcpy, width, i - 1, find, target);
+		map_flood_fill(mapcpy, width, i - width, find);
+		map_flood_fill(mapcpy, width, i + 1, find);
+		map_flood_fill(mapcpy, width, i + width, find);
+		map_flood_fill(mapcpy, width, i - 1, find);
 	}
 }
 
-int can_reach_target(t_game *g, char target)
+int can_reach_target(t_game *g)
 {
 	char	*mapcpy;
+	char	target;
 	int		find;
 	int		i;
 
+	target = 'E';
 	mapcpy = ft_strdup(g->map);
 	if (target == 'E')
 		find = 1;
@@ -46,13 +48,53 @@ int can_reach_target(t_game *g, char target)
 			break;
 		i++;
 	}
-	map_flood_fill(mapcpy, g->width, i, &find, target);
+	map_flood_fill(mapcpy, g->width, i, &find);
+	free(mapcpy);
+	return (find <= 0);
+}
+void	map_flood_fill_c(char *mapcpy, int width, int i, int *find)
+{
+	if (mapcpy[i] == '1' || mapcpy[i] == '*' || *find == 0 || mapcpy[i] == 'E')
+	return ;
+	if (mapcpy[i] == 'C')
+		*find = *find - 1;
+	mapcpy[i] = '*';
+	if (*find > 0)
+	{
+		map_flood_fill_c(mapcpy, width, i - width, find);
+		map_flood_fill_c(mapcpy, width, i + 1, find);
+		map_flood_fill_c(mapcpy, width, i + width, find);
+		map_flood_fill_c(mapcpy, width, i - 1, find);
+	}
+}
+
+int can_reach_target_c(t_game *g)
+{
+	char	*mapcpy;
+	char	target;
+	int		find;
+	int		i;
+
+	target = 'C';
+	mapcpy = ft_strdup(g->map);
+	if (target == 'E')
+		find = 1;
+	else
+		find = coll_count(g->map);
+	i = 0;
+    while (i < ft_strlen(g->map))
+	{
+		if (g->map[i] == 'P')
+			break;
+		i++;
+	}
+	map_flood_fill_c(mapcpy, g->width, i, &find);
 	free(mapcpy);
 	return (find <= 0);
 }
 
 void map_possible_arrival(t_game *g)
 {
-    if (!can_reach_target(g, 'E') && !can_reach_target(g, 'C'))
+    if (!can_reach_target(g) && !can_reach_target_c(g))
         print_error("Cannot reach the exit or any key!");
 }
