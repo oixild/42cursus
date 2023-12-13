c5631786f985e4ca83ceacebd8ebd1cb859b4a67
David Sabater 42 <dsabater@student.42barcelona.com>
2023-12-13 11:18:01 +0100

    seg parched

--- so_long/src/errors.c
+++ so_long/src/errors.c
@@ -6,7 +6,7 @@
 /*   By: dsabater <dsabater@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 13:26:31 by dsabater          #+#    #+#             */
-/*   Updated: 2023/12/12 12:07:06 by dsabater         ###   ########.fr       */
+/*   Updated: 2023/12/13 11:14:20 by dsabater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ void	check_args(int argc, char **argv)
 {
 	int		fd;
 
-	if (!argv[1][0])
-		print_error("Error\nYou need to put a correct file path!");
 	if (argc != 2)
 		print_error("Error\nInvalid number of arguments!");
+	if (!argv[1][0])
+		print_error("Error\nYou need to put a correct file path!");
 	fd = open(argv[1], O_RDONLY);
 	if (ft_strrncmp(argv[1], ".ber", 4))
 		print_error("Error\nInvalid file type, use .ber!");
